7b6b79f959db6c5acb2f6cb2e50a635407b154db
Sambhav-3010 <sambhavmani.tripathi@gmail.com>
2024-09-20 12:57:41 +0530

    heyy

--- prediction_model.py
+++ prediction_model.py
@@ -4,8 +4,6 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.inception_v3 import preprocess_input
 from tensorflow.keras.models import load_model
-import locale
-import sys
 
 def predict_disease():
     try:
